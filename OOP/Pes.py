42043f0a324c01e2bb2e60a63d379301614a4940
hrosicka <hrosicka@post.cz>
2022-12-20 11:22:32 +0100

    test commitu

---
+++ OOP/Pes.py
@@ -0,0 +1,50 @@
+def main():
+
+    # ucime se python
+    print('Tento soubor je o psech!')
+    
+    # stenatko Alik
+    Alik = MyDog('Alík','Voříšek', 1)
+    Alik.show_dog()
+    Alik.zastekej()
+    Alik.stari()
+
+    # Azor je pes v plné síle
+    Azor = MyDog('Azor','Vlčák', 10)
+    Azor.show_dog()
+    Azor.zastekej()
+    Azor.stari()
+
+    # Prcek je čivava v důchodu
+    Prcek = MyDog('Prcek','Čivava', 12)
+    Prcek.show_dog()
+    Prcek.zastekej()
+    Prcek.stari()
+
+
+class MyDog:
+    def __init__(self, name, breed, age):
+        self.name = name
+        self.breed = breed
+        self.age = age
+
+ 
+    def show_dog(self):
+        print("Muj pes se jmenuje", self.name, "a je to", self.breed)
+
+    def zastekej(self):
+        print(self.name, 'steka HAF HAF')
+    
+    def stari(self):
+        if (self.age <= 2):
+            print(self.name, "je jeste stene")
+        elif (self.age <= 10):
+            print(self.name, "je pes v plené síle")
+        else:
+            print(self.name, "je už starší pejsek")
+
+
+
+
+if __name__ == '__main__':
+    main()
