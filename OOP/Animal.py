c414f4e18c70c3143ecb47ddb368e1dce69934ab
hrosicka <hrosicka@post.cz>
2022-12-23 12:55:32 +0100

    inheritance and override method

--- OOP/Animal.py
+++ OOP/Animal.py
@@ -7,16 +7,28 @@ def main():
     print('Animals!')
     
     # dog Azor
+    print()
     dog1 = Dog("Azor", 2)
     dog1.show_animal()
     dog1.eat()
     dog1.bark()
+    dog1.make_nois()
 
     # cat Micka
+    print()
     cat1 = Cat("Micka", 4)
     cat1.show_animal()
     cat1.eat()
     cat1.meow()
+    cat1.make_nois()
+
+    # pet Archi
+    print()
+    pet1 = Pet("Archi", 3)
+    pet1.show_animal()
+    pet1.eat()
+    pet1.make_nois()
+
 
 
 # parent class Pet
@@ -32,18 +44,29 @@ class Pet:
     def show_animal(self):
         print ("Name:", self.name, "age:", self.age)
 
+    def make_nois(self):
+        print("{} says, VrrrVrrr".format(self.name))
+
 #children class Dog
 class Dog(Pet):
 
     def bark(self):
-        print('Bark!!!!')
+        print("Bark!!!!")
+    
+    # override method
+    def make_nois(self):
+        print("{} says, Bark!!!!".format(self.name))
 
 
 #children class Cat
 class Cat(Pet):
 
     def meow(self):
-        print('Meow!!!!')
+        print("Meow!!!!")
+
+    # override method
+    def make_nois(self):
+        print("{} says, Meow!!!!".format(self.name))
 
 
 
