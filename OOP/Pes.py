f5e9a33c665dedc9485db5b5b0162d30b0086acf
hrosicka <hrosicka@post.cz>
2023-01-03 15:30:26 +0100

    Inheritance, for cycle, while cycle

--- OOP/Pes.py
+++ OOP/Pes.py
@@ -2,26 +2,28 @@ def main():
     print('Tento soubor je o psech!')
     
     # stenatko Alik
-    Alik = MyDog('Alík','Voříšek', 1)
+    Alik = MyCuteDog('Alík','Voříšek', 1)
     Alik.show_dog()
-    Alik.zastekej()
+    Alik.zastekej(5)
     Alik.stari()
+    Alik.be_cute()
 
     # Azor je pes v plné síle
-    Azor = MyDog('Azor','Vlčák', 10)
+    Azor = MyAngryDog('Azor','Vlčák', 10)
     Azor.show_dog()
-    Azor.zastekej()
+    Azor.zastekej(2)
     Azor.stari()
+    Azor.be_angry(3)
 
     # Prcek je čivava v důchodu
     Prcek = MyDog('Prcek','Čivava', 12)
     Prcek.show_dog()
-    Prcek.zastekej()
+    Prcek.zastekej(1)
     Prcek.stari()
 
 
 class MyDog:
-    zvuk = "Haf Haf"
+    zvuk = "Haf"
 
     def __init__(self, name, breed, age):
         self.name = name
@@ -30,10 +32,16 @@ class MyDog:
 
  
     def show_dog(self):
+        print("\n-----")
         print("Muj pes se jmenuje", self.name, "a je to", self.breed)
+        print("-----")
 
-    def zastekej(self):
-        print(self.name, "dělá", self.zvuk)
+    def zastekej(self, count):
+        print(self.name, "dělá:")
+        i = 0
+        while i < count:
+            print(self.zvuk)
+            i += 1
     
     def stari(self):
         if (self.age <= 2):
@@ -42,6 +50,23 @@ class MyDog:
             print(self.name, "je pes v plené síle")
         else:
             print(self.name, "je už starší pejsek")
+
+# inheritance
+class MyAngryDog(MyDog):
+
+    def be_angry(self, count):
+        print("{} is so angry!!!!".format(self.name))
+        for i in range(count):
+            print("VRRrrr")
+
+
+
+
+# inheritance
+class MyCuteDog(MyDog):
+
+    def be_cute(self):
+        print("{} is so cute!!!!".format(self.name))
 
 
 
